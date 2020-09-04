95dfce252377d85cf399796460524d9df6448b78
umarjabbar <umarjuabbar@gmail.com>
2020-09-04 23:01:57 +0700

    Intall React

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -19,6 +19,34 @@ function App() {
           Learn React
         </a>
       </header>
+       */}
+       <div className="Kotak">
+          <h1>Form Pembelian Buah</h1>
+          <div className="Konten">
+            <div className="Atas">
+              <div className="Nama-pelanggan">Nama Pelanggan</div>
+              <input className="Input-name"></input>
+            </div>
+            <div className="Bawah">
+              <div className="Daftar-item">Daftar Item</div>
+              <div className="Item-list">
+              <input type="checkbox" name="semangka" value="semangka"></input>
+              <label>Semangka </label><br/>
+              <input type="checkbox" name="jeruk" value="jeruk"></input>
+              <label>Jeruk </label><br/>
+              <input type="checkbox" name="nanas" value="nanas"></input>
+              <label>Nanas</label><br/>
+              <input type="checkbox" name="salak" value="salak"></input>
+              <label>Salak</label><br/>
+              <input type="checkbox" name="anggur" value="anggur"></input>
+              <label>Anggur</label><br/><br/>
+              </div>
+            </div>
+            <button className="Btn-submit">
+              <a href="#">Kirim</a>
+            </button>
+          </div>
+       </div>
     </div>
   );
 }
